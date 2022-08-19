508cc128bc431d5d0f721291ae56aed1105830a2
Sandeep Sahani <sandeepsahani76j@gmail.com>
2022-08-19 21:49:49 +0530

    Added HelloWorld.java file

---
+++ Basics/HelloWorld.java
@@ -0,0 +1,19 @@
+/* 01-Basic java program structure and compilation */
+
+// javac programName.java (Compilation)
+// java programName (Execution)
+
+class HelloWorld 
+{
+     String name;
+     String age;
+     static String language; // Static variable
+     boolean isProgrammer; // Instance variable
+    public static void main(String[] args) // Execution stsrts from here ! 
+    {
+        int localVariable=10; // Local variable
+        System.out.println("Hello World"); // Prints "Hello World on console"
+        System.out.println("Local variable value is "+localVariable);
+    }
+ 
+}
