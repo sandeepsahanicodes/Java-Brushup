7107b4a63b526215405576534785f28410f0fedd
Sandeep Sahani <sandeepsahani76j@gmail.com>
2022-08-19 21:26:54 +0530

    First commit

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test 
+{
+      public static void main(String[] args) 
+      {
+        Rectangle r = new Rectangle();
+        r.area();
+        r.peri();
+        r.disp();
+      }
+}
