f075f4edd7b85bab2d26125c49bcfab2c1e47e56
Sandeep Sahani <sandeepsahani76j@gmail.com>
2022-08-21 00:46:34 +0530

    Added new files

---
+++ Basics/Grammer.java
@@ -0,0 +1,18 @@
+
+/* 02-Basic grammer of java*/
+class Grammer 
+{
+    public static void main(String[] args) 
+    {
+        int num1=10;
+        int num2=20;
+        int sum,multiply;
+        sum=num1+num2;
+        multiply=num1*num2;
+        System.out.println("First number= "+num1);
+        System.out.println("Second number= "+num2);
+        System.out.println("Sum= "+sum);
+        System.out.println("Multiplay= "+multiply);
+
+    }
+}
---
+++ Basics/InputUsingScanner.java
@@ -0,0 +1,26 @@
+import java.util.Scanner; // Importing Scanner class
+
+class InputUsingScanner
+{
+    public static void main(String[] args) 
+    {
+        int num1=0,num2=0;
+        int add,multiply;
+
+        Scanner sc= new Scanner(System.in); // Instance of Scanner class
+        System.out.println("Enter first number");
+        num1=sc.nextInt();
+        System.out.println("Enter second number");
+        num2=sc.nextInt();
+
+        add=num1+num2;
+        multiply=num1*num2; 
+        
+        System.out.println("First number="+num1);
+        System.out.println("Second number="+num2);
+        System.out.println("Sum="+add);
+        System.out.println("Multiply="+multiply);
+        sc.close(); // Closing Scanner object
+
+    }
+}
