592b106823f9d968ac90f3b4ea4ff612cd971ad7
Sandeep Sahani <sandeepsahani76j@gmail.com>
2022-08-22 17:05:23 +0530

    Modified this file

--- OOPS Concepts/RectangleDriver.java
+++ OOPS Concepts/RectangleDriver.java
@@ -1,7 +1,25 @@
 class RectangleDriver 
 {
-    int length;
-    int breadth;
+    private int length;
+    private int breadth;
 
-    
+    void init(int length,int breadth)
+    {
+        this.length=length;
+        this.breadth=breadth;
+    }
+    void display()
+    {
+        System.out.println("Length= "+length+"Breadth= "+breadth);
+    }
+    void calculateArea()
+    {
+        System.out.println("Area= "+(length*breadth)+"Sq Unit");
+    }
+    void calculatePerimeter()
+    {
+        System.out.println("Perimeter= "+2*(length+breadth)+"CM");
+    }
+
+
 }
