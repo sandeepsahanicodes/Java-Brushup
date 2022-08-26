6f19fa9735d00a226fb9889795a4cbde5b9f8199
sandeepsahanicodes <sandeepsahanigithub@gmail.com>
2022-08-27 02:05:22 +0530

    Added comment

--- OOPS Concepts/Classes & Objects/RectangleDriver.java
+++ OOPS Concepts/Classes & Objects/RectangleDriver.java
@@ -1,4 +1,19 @@
 /* 04-Learned about classes and object*/
+
+/* 
+   ~ Class is blueprint for an object.
+   ~ Class doesn't occupies space in memory its the object which which occpies memory.
+   ~ Syntax 
+     class ClassName // class syntax
+     {
+        // properties(Variables)
+        // Behaviours(Methods)
+     }
+     
+     ClassName objectName = new ClassName(); // object creation
+   ~ Its recommended to write class name in Pascel case.
+*/
+
 class Rectangle
 {
     private int length;
