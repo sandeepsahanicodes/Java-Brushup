00c873b17fbdf1b3912f3779233342c71feb734a
sandeepsahanicodes <sandeepsahanigithub@gmail.com>
2022-09-01 23:55:23 +0530

    Added file

---
+++ OOPS Concepts/this Keyword/AddComplexNumber.java
@@ -0,0 +1,42 @@
+class Complex
+{
+    private int real,imaginary;
+
+    Complex()
+    {
+        real=0;
+        imaginary=0;
+    }
+
+    Complex(int real,int imaginary)
+    {
+        this.real=real;
+        this.imaginary=imaginary;
+    }
+
+    void display()
+    {
+        System.out.println("Complex number is "+real+"+"+imaginary+"i");
+    }
+
+    void add(Complex c2)
+    {
+        Complex c3=new Complex();
+        c3.real = this.real+c2.real;
+        c3.imaginary = this.imaginary+c2.imaginary;
+    }
+
+}
+
+class AddComplexNumber
+{
+    public static void main(String[] args) 
+    {
+        Complex c1 = new Complex(4,5);
+        Complex c2 = new Complex(6, 7);
+        c1.display();
+        c2.display();
+        c1.add(c2);
+        c2.display();
+    }
+}
