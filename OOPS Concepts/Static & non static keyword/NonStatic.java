8f9076da357d2c89642a6a833e21fc1a0e94e9d3
sandeepsahanicodes <sandeepsahanigithub@gmail.com>
2022-09-13 19:48:25 +0530

    Added cde

--- OOPS Concepts/Static & non static keyword/NonStatic.java
+++ OOPS Concepts/Static & non static keyword/NonStatic.java
@@ -1,5 +1,30 @@
 /* 13- About non static concept */
 class NonStatic 
 {
+    int x; // Non static instance variable
+    NonStatic() // Default constructor
+    {
+        System.out.println("Default constructor");
+    }
     
+    // Non static block
+    {
+       System.out.println("Non static block 1");
+    }
+    // Non static method
+    void m1()
+    {
+        System.out.println("Non static method 1");
+    }
+    // Non static block
+    {
+        System.out.println("Non static block 2");
+    }
+    
+    // Driver method
+    public static void main(String[] args) 
+    {
+        NonStatic ns = new NonStatic(); // Instance of class NonStatic
+        ns.m1(); // Method call
+    }
 }
