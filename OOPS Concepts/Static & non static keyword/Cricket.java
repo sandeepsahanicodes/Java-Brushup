f180b454e463139507dc8e0a281eeb395645b00f
sandeepsahanicodes <sandeepsahanigithub@gmail.com>
2022-09-14 22:50:32 +0530

    Added new file

---
+++ OOPS Concepts/Static & non static keyword/Cricket.java
@@ -0,0 +1,30 @@
+/* 14- Program for illustrate difference between static and non static memebers*/
+class Cricket 
+{
+    String name; // Player name
+    int individualScore;
+    static int teamScore;
+
+    Cricket(String name ,int individualScore) // Parameterised constructor for taking players input
+    {
+        this.name=name;
+        this.individualScore=individualScore;
+        teamScore = teamScore+individualScore;
+    }
+
+    void display() // Display players details
+    {
+        System.out.println(name+" "+individualScore);
+    }
+
+    // Driver method
+    public static void main(String[] args) 
+    {
+        Cricket c1 = new Cricket("Virat", 100);
+        Cricket c2 = new Cricket("Rohit", 80);
+        c1.display();
+        c2.display();
+        System.out.println("Team score="+Cricket.teamScore);
+    }
+
+}
