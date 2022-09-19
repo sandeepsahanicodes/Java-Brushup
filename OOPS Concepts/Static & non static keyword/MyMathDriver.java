fd4b888902ae812c540b1fa43d0860a48f7bc3f9
sandeepsahanicodes <sandeepsahanigithub@gmail.com>
2022-09-19 19:34:08 +0530

    Added new file

---
+++ OOPS Concepts/Static & non static keyword/MyMathDriver.java
@@ -0,0 +1,40 @@
+/*18- Utility class concept*/
+/*
+  ~ Class which contains only static members called as Utility class
+  ~ Non need of creation of object of an utility class, methods can be 
+    accessed using class name only
+*/
+class MyMath
+{
+    static int add(int a,int b) 
+    {
+       return a+b;
+    }
+
+    static int sub(int a,int b)
+    {
+       return a-b;
+    }
+
+    static int product(int a,int b)
+    {
+       return a*b;
+    }
+
+    static int division(int a,int b)
+    {
+       return a/b;
+    }
+}
+// Driver class
+class MyMathDriver 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Addition of 10 and 20 is "+MyMath.add(10, 20)); // Used class name to call methods
+        System.out.println("Subtraction of 10 and 20 is "+MyMath.sub(10, 20));
+        System.out.println("Product of 10 and 20 is "+MyMath.product(10, 20));
+        System.out.println("Division of 10 and 20 is "+MyMath.division(10, 20));
+
+    }
+}
