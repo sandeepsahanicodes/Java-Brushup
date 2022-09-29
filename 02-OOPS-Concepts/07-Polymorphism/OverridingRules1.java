4e68272d5a35df98356e96bc0fcb377dbf03e212
sandeepsahanicodes <sandeepsahanigithub@gmail.com>
2022-09-29 23:13:45 +0530

    Add new file

---
+++ 02-OOPS-Concepts/07-Polymorphism/OverridingRules1.java
@@ -0,0 +1,36 @@
+/*31- Overriding rules*/
+/*
+ ~ We can't overriding varible.We can only override methods.
+ ~ When instance variable in child class and parent class is
+   same then the class which reference is created that class 
+   variable will get accessed.
+ */
+class Parent
+{
+   int x=10; // Parent instance variable
+   void m1()
+   {
+     System.out.println("Parent class m1 method");
+   }
+}
+
+class Child extends Parent
+{
+   int x=20; // Child instance variable
+   @Override
+   void m1()
+   {
+     System.out.println("Child class m1 method");
+   }
+}
+// Driver class
+class OverridingRules1 
+{
+    public static void main(String[] args)
+    {
+        Child childHoldsChild=new Child();
+        System.out.println("x="+childHoldsChild.x); // Childs instance variable gets accessed as reference is of child.
+        Parent parentHoldsChild=new Child();
+        System.out.println("x="+parentHoldsChild.x); // Reference's instance variable will gets accessed(i.e Parents x).
+    }
+}
